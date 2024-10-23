1acd89d1291b1962cb8ccc91263935215e9761ef
Mateus Lima <Mateus Lima>
2024-10-23 12:52:09 -0300

    adicionado outras telas

---
+++ Desktop/figma-to-html (1)/js/script.js
@@ -0,0 +1,15 @@
+var campoUsuario = document.getElementById("usuario");
+var campoSenha = document.getElementById("senha");
+
+function iniciarSessao(){
+
+    // if (campoUsuario.value != "admin"){
+    //     alert("Campo inválido")
+    // } else if (campoSenha.value != "senha123"){
+    //     alert("Senha inválida")
+    // } else {
+    //     alert("Login realizado com sucesso!")
+    // }
+
+    ;
+}
